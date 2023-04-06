bb5cf28968713094df5c40f5cc4b81a16bbc1848
Caleb Schmidt <cschmi14@gmail.com>
2023-04-06 12:48:57 -0400

    Done

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 4; i++)
+            {
+                RonVSKanyeAPI.Kanye();
+
+                RonVSKanyeAPI.Ron();
+            }
+
         }
     }
 }
--- APIsAndJSON/Quote.cs
+++ APIsAndJSON/Quote.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    public class OpenWeatherMapAPI
+    internal class Quote
     {
     }
 }
